76afdba07f0f90b428c910e69aa31f6c58f1e2c6
philippeSousa <philippesousa93@gmail.com>
2015-04-20 12:17:11 +0200

    sqdsq

--- Vues/selectAlbum.php
+++ Vues/selectAlbum.php
@@ -1,7 +1,7 @@
 <?php	
 error_reporting(E_ALL);
   ini_set("display_errors", 1);
-  require 'vendor/autoload.php';
+  require '../vendor/autoload.php';
   session_start();
 
   use Facebook\FacebookSession;
